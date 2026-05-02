8c6fce5375a2c941bc284b10b02995e612243826
pshruthi23 <pshruthi2305@gmail.com>
2026-05-01 23:48:39 -0500

    Create Preprocessing code.sql

---
+++ chocolate/Preprocessing code.sql
@@ -0,0 +1,11 @@
+--Example for the stores table
+
+ALTER TABLE `chocolate-493502.Dataset.stores`
+RENAME COLUMN string_field_0 to store_id,
+RENAME COLUMN string_field_1 to store_name,
+RENAME COLUMN string_field_2 to city,
+RENAME COLUMN string_field_3 to country, 
+RENAME COLUMN string_field_4 to store_type,
+--RENAME COLUMN string_field_5 to quantity,
+--RENAME COLUMN string_field_6 to quantity,
+--RENAME COLUMN string_field_7 to quantity,
